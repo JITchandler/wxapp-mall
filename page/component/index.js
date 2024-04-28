8446a7484c396bbd9f1527ab64e1ee0c51bef4f8
chandlerzzz <24572223665@qq.com>
2024-04-28 09:08:17 +0800

    Initial Commit

---
+++ page/component/index.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    imgUrls: [
+      '/image/fruitSwiper-1.png',
+      '/image/fruitSwiper-2.png',
+      '/image/fruitSwiper-3.png'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 3000,
+    duration: 800,
+  }
+})
---
+++ page/component/paysuccess/paysuccess.js
@@ -0,0 +1,10 @@
+
+Page({
+  //返回订单详情
+  handleViewOrder()
+  {
+    wx.redirectTo({
+      url: '/page/component/orders/orders',
+    })
+  }
+})
